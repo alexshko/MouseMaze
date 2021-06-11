a6ea246bc187763454425b5af2bc62d9309308cb
alexshko <alexshko@gmail.com>
2021-06-11 21:52:12 +0300

    joystick integration works

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace alexshko.colamazle.core
+{
+
+    public class GameController : MonoBehaviour
+    {
+        public static GameController Instance { get; set; }
+
+        public DynamicJoystick JoystickPref;
+
+        public float JoystickValue
+        {
+            get => JoystickPref.Vertical;
+           
+        }
+
+        private void Awake()
+        {
+            Instance = this;
+        }
+    }
+}
--- Assets/Scripts/MouseyController.cs
+++ Assets/Scripts/MouseyController.cs
@@ -1,54 +1,61 @@
-﻿using System.Collections;
+﻿using alexshko.colamazle.core;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEditor.Animations;
 using UnityEngine;
 
-public class MouseyController : MonoBehaviour
+namespace alexshko.colamazle.Entities
 {
-    public float MaxForwardSpeed = 0f;
-    public float MaxBackwardSpeed = 0f;
-
-
-    private CharacterController character;
-    private Vector3 MoveToMake;
-
-    private void Start()
+    public class MouseyController : MonoBehaviour
     {
-        character = GetComponent<CharacterController>();
-        if (!character)
+        public float MaxForwardSpeed = 0f;
+        public float MaxBackwardSpeed = 0f;
+
+
+        private CharacterController character;
+        private Vector3 MoveToMake;
+
+        private void Start()
         {
-            Debug.LogError("missing CharacterController");
+            character = GetComponent<CharacterController>();
+            if (!character)
+            {
+                Debug.LogError("missing CharacterController");
+            }
+
         }
 
-    }
-
-    private void Update()
-    {
-        MoveToMake = Vector3.zero;
-        if (Mathf.Abs(Input.GetAxis("Vertical")) >0.3f)
+        private void Update()
         {
-            Debug.Log("Going forward");
-            MoveToMake.z += (Input.GetAxis("Vertical") > 0? MaxForwardSpeed : MaxBackwardSpeed) * Input.GetAxis("Vertical");
+            MoveToMake = Vector3.zero;
+            //take the Vertical input axis. and also the vertical valuue of the joystick:
+            float InputVal = Input.GetAxis("Vertical") + GameController.Instance.JoystickValue;
+            if (Mathf.Abs(InputVal) > 0.05f)
+            {
+                Debug.Log("Going forward");
+                MoveToMake.z += (InputVal > 0 ? MaxForwardSpeed : MaxBackwardSpeed) * InputVal;
+            }
+
+            //add gravity to the speed
+            if (character.isGrounded)
+            {
+                Debug.Log("Character grounder");
+            }
+            MoveToMake += character.isGrounded ? Vector3.zero : Physics.gravity;
         }
 
-        //add gravity to the speed
-        if (character.isGrounded) {
-            Debug.Log("Character grounder");
-        }
-        MoveToMake += character.isGrounded ? Vector3.zero : Physics.gravity;  
-    }
-
-    private void FixedUpdate()
-    {
-        if (MoveToMake != Vector3.zero)
+        private void FixedUpdate()
         {
-            character.Move(MoveToMake * Time.deltaTime);
+            if (MoveToMake != Vector3.zero)
+            {
+                character.Move(MoveToMake * Time.deltaTime);
+            }
+            MakeMoveAnim(MoveToMake.z);
         }
-        MakeMoveAnim(MoveToMake.z);
-    }
 
-    private void MakeMoveAnim(float speed)
-    {
-        character.GetComponent<Animator>().SetFloat("Speed",Mathf.Clamp(speed, -MaxBackwardSpeed, MaxForwardSpeed));
+        private void MakeMoveAnim(float speed)
+        {
+            character.GetComponent<Animator>().SetFloat("Speed", Mathf.Clamp(speed, -MaxBackwardSpeed, MaxForwardSpeed));
+        }
     }
 }
