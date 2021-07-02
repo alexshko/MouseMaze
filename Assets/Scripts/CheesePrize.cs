8d053d2d8984cba167927928ee2a2c57c3ac3295
alexshko <alexshko@gmail.com>
2021-07-02 10:10:11 +0300

    .

---
+++ Assets/Scripts/CheesePrize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace alexshko.colamazle.Entities
+{
+    public class CheesePrize : MonoBehaviour
+    {
+        private void OnTriggerEnter(Collider other)
+        {
+            CollectCheese();
+            Debug.Log("CheeseCollected");
+        }
+
+        private void CollectCheese()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
