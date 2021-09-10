1601c357ae5924928432ae17e9a404a87ecb4015
alexshko <alexshko@gmail.com>
2021-09-10 21:29:53 +0300

    .

--- Assets/Scripts/TutorialCheckPoint.cs
+++ Assets/Scripts/TutorialCheckPoint.cs
@@ -5,12 +5,14 @@ using UnityEngine.UI;
 public class TutorialCheckPoint : MonoBehaviour
 {
     public string messageToShow;
+    private bool isVisited = false;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.tag == "Player")
+        if (other.tag == "Player" && !isVisited)
         {
             TutorialCheckPointEngine.ShowMessageUI(messageToShow);
+            isVisited = true;
         }
     }
 }
