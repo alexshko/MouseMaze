254637bc28922c9833247185b82986deea97454e
alexshko <alexshko@gmail.com>
2021-09-30 23:12:30 +0300

    Rock

---
+++ Assets/Scripts/FloatingRocks/FloatingRock.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FloatingRock : MonoBehaviour
+{
+    public float speedOfMove;
+    public bool isMooving { get; set; }
+
+    private Rigidbody rb;
+
+
+    protected abstract void ApplyForce();
+
+    public void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+    public void Update()
+    {
+        if (isMooving)
+        {
+            ApplyForce();
+        }
+    }
+
+}
